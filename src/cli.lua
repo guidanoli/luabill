feae56743b1366771203b5d4a9e1835a585627d8
Guilherme Dantas <guidanoli@proton.me>
2024-02-07 12:30:57 -0300

    feat: simplify loading (don't store node structure)

--- src/cli.lua
+++ src/cli.lua
@@ -6,8 +6,7 @@ local help = [[
 luabill - organize your bills with Lua
 Usage: luabill <command> [args...]
 Commands:
-  dump - Dump node
-    [--flatten] - Flatten node
+  dump - Dump bills
 ]]
 
 local function usage ()
@@ -40,27 +39,9 @@ end
 
 local cmd = args[i]
 if cmd == 'dump' then
-    local node = luabill:load(dir)
-    i = i + 1
-    local flatten = false
-    while true do
-        local arg = args[i]
-        if arg == '--flatten' then
-            flatten = true
-            i = i + 1
-        elseif arg == nil then
-            break
-        else
-            usage()
-        end
-    end
-    if flatten then
-        local array = {}
-        node:flatten(array, '.')
-        print(serpent.block(array, { comment = false }))
-    else
-        print(serpent.block(node, { comment = false }))
-    end
+    local bills = {}
+    luabill:loadbills(dir, bills)
+    print(serpent.block(bills, { comment = false }))
 else
     usage()
 end
--- src/luabill.lua
+++ src/luabill.lua
@@ -1,35 +1,31 @@
 local lfs = require 'lfs'
 
-local Node = require 'Node'
 local Bill = require 'Bill'
 
 local luabill = {}
 
-function luabill:load (path)
-    local billpath = path .. '/bill.lua'
-    local f = loadfile(billpath)
-    if f ~= nil then
-        local t = f()
+function luabill:loadbills (path, bills)
+    local loadt = loadfile(path .. '/bill.lua')
+    if loadt ~= nil then
+        local t = loadt()
         local bill, err = Bill:new(t)
         if not bill then
-            error(string.format('"%s": %s', billpath, err))
+            error(string.format('"%s": %s', path, err))
         end
-        return Node:new(bill)
+        table.insert(bills, {
+            bill = bill,
+            path = path,
+        })
     else
-        local t = {}
         for file in lfs.dir(path) do
             if file ~= '.' and file ~= '..' then
-                local f = path .. '/' .. file
-                local attr = assert(lfs.attributes(f))
+                local childpath = path .. '/' .. file
+                local attr = assert(lfs.attributes(childpath))
                 if attr.mode == 'directory' then
-                    local node = self:load(f)
-                    if node.children == nil or next(node.children) ~= nil then
-                        t[file] = node
-                    end
+                    self:loadbills(childpath, bills)
                 end
             end
         end
-        return Node:new(nil, t)
     end
 end
 
