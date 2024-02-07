e9d1a946fc50dae6de0f8984b1bb889e0924fef2
Guilherme Dantas <guidanoli@proton.me>
2024-02-07 12:59:08 -0300

    feat: index bill by path

--- luabill/fs.lua
+++ luabill/fs.lua
@@ -12,10 +12,7 @@ function fs:loadbills (path, bills)
         if not bill then
             error(string.format('"%s": %s', path, err))
         end
-        table.insert(bills, {
-            bill = bill,
-            path = path,
-        })
+        bills[path] = bill
     else
         for file in lfs.dir(path) do
             if file ~= '.' and file ~= '..' then
